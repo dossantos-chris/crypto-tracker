ea558c4a70d1e477b539299d2fc8f90d4a732c48
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-11 22:04:13 -0400

    Added comments and restructured getData function

--- main/btc.js
+++ main/btc.js
@@ -2,11 +2,12 @@ require('dotenv').config()
 const axios = require('axios');
 const fs = require('fs');
 
+//Pull the API URL and key from env files
 const API_URL = process.env.API_URL
 const API_KEY = process.env.API_KEY
 
-async function getData()
-{   
+//Send Get request to CoinMarketCap API for BTC quote
+const getData = async () => {
     try 
     {
         let response = await axios.get(API_URL, 
@@ -22,10 +23,12 @@ async function getData()
     {
         return error;
     }
-}
+};
 
-getData().then((data) => 
+//Console log the current price of BTC
+getData().then((data) =>
     fs.writeFile("btc.json", JSON.stringify(data, null, 4), (err) => 
         console.log("data written to file"))
 );
 
+//data['data']['1']['quote']['USD']['price'] to get current price of BTC
