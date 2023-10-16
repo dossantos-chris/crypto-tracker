fc2f5dfc08bf3e70ea48f6a2f5492ad85306a76e
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-16 16:43:38 -0400

    Implemented Express server with Cors to act as a backend

--- server/api/getData.js
+++ server/api/getData.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 
-//Pull the API URL and key from env files
+// Pull the API URL and API key from env file
 const API_URL = process.env.API_URL;
 const API_KEY = process.env.API_KEY;
 
-//Send Get request to CoinMarketCap API for BTC quote
+// Send GET request to CoinMarketCap API for BTC quote
 module.exports.getData = async () => {
     try 
     {
--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 require('dotenv').config();
-const axios = require('axios');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
 const { getData } = require('./api/getData.js');
 
-//Output BTC stats to json file
-getData().then((data) =>
-    console.log(data['data']['1']['quote']['USD'])
-);
+app.use(cors('*'));
 
-//data['data']['1']['quote']['USD']['price'] to get current price of BTC
+// Sends user current stats on the price of BTC
+app.get('/api/price', (req, res) => {
+
+    // Fetch current BTC quote from getData API
+    getData().then((data) =>
+        res.json(data['data']['1']['quote']['USD']));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+// data['data']['1']['quote']['USD']['price'] to get current price of BTC
