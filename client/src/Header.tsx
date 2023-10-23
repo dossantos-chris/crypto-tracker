250e5558e73a14c66bd9cd520fcb3fdeaa7d3d2c
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-23 14:58:10 -0400

    Added Header

---
+++ client/src/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <div className="container">
+        <div>
+          <a href="/">Crypto Tracker</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
