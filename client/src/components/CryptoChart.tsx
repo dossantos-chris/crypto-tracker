dd1a50214482146f16b5773ed8a8811eb7eeaf8f
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-24 15:05:41 -0400

    Began implementing CryptoChart using Chart.js

---
+++ client/src/components/CryptoChart.tsx
@@ -0,0 +1,12 @@
+import Chart from 'chart.js/auto';
+import './CryptoChart.css'
+
+const CryptoChart = () => {
+  
+
+    return (
+    <div className='chart-container'>CryptoChart</div>
+  )
+}
+
+export default CryptoChart
