a6a426488af13fbb48e5c8ef7dd6f55a396595f9
Chris Dos Santos <dossantos.chris13@gmail.com>
2023-10-24 15:31:39 -0400

    Implemented and styled working test chart

--- client/src/components/CryptoChart.tsx
+++ client/src/components/CryptoChart.tsx
@@ -1,12 +1,45 @@
 import Chart from 'chart.js/auto';
 import './CryptoChart.css'
+import { useEffect } from 'react';
 
 const CryptoChart = () => {
   
+  useEffect(() => {
+    const ctx = document.getElementById("myChart") as HTMLCanvasElement;
+    if (!ctx) return;
 
-    return (
-    <div className='chart-container'>CryptoChart</div>
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+          {
+            label: "Bitcoin Price",
+            data: [30000, 35000, 40000, 45000, 42000, 50000],
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "#F7931A",
+            borderWidth: 2,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false,
+          },
+        },
+      },
+    });
+    return () => {
+      myChart.destroy();
+    };
+  }, []);
+
+  return(
+    <div className="chart-container">
+      <canvas id="myChart"></canvas>
+    </div>
   )
-}
+};
 
 export default CryptoChart
